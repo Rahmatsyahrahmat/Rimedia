056c5ab5fe6b0c143a6caf0cde24f8989ac7a96b
Rahmatsyahrahmat <rahmatsyah1317@gmail.com>
2019-09-03 03:14:53 +0700

    Register Ui

---
+++ app/src/main/java/com/rahmatsyah/rimedia/view/activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.rahmatsyah.rimedia.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rahmatsyah.rimedia.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
