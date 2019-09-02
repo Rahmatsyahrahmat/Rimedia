604d48db5fd73c43a914a1760f89fb3f2adfa3b4
Rahmatsyahrahmat <rahmatsyah1317@gmail.com>
2019-09-03 01:54:43 +0700

    Ui Login

--- app/src/main/java/com/rahmatsyah/rimedia/view/activity/MainActivity.java
+++ app/src/main/java/com/rahmatsyah/rimedia/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.rahmatsyah.rimedia;
+package com.rahmatsyah.rimedia.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.rahmatsyah.rimedia.R;
 
 public class MainActivity extends AppCompatActivity {
 
