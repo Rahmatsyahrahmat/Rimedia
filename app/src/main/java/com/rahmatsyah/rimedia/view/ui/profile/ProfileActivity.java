fe2174337208ce5625dd3f46c05996784e6b0b91
Rahmatsyahrahmat <rahmatsyah1317@gmail.com>
2019-09-04 21:46:47 +0700

    Profile App Bar

--- app/src/main/java/com/rahmatsyah/rimedia/view/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/rahmatsyah/rimedia/view/ui/profile/ProfileActivity.java
@@ -1,18 +1,80 @@
 package com.rahmatsyah.rimedia.view.ui.profile;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.rahmatsyah.rimedia.R;
 import com.rahmatsyah.rimedia.utils.BottomNavigationViewHelper;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         BottomNavigationViewHelper.getIntance(this,this).enableNavigation();
+
+        Toolbar toolbar = findViewById(R.id.toolBarProfile);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawerLayoutProfile);
+        NavigationView navigationView = findViewById(R.id.navViewProfile);
+        final CoordinatorLayout content = findViewById(R.id.appBarProfile);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close){
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                float slideX = drawerView.getWidth() * slideOffset;
+                content.setTranslationX(-slideX);
+            }
+        };
+        toggle.setDrawerIndicatorEnabled(false);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ImageView ivToggleMenu = findViewById(R.id.ivToggleMenuProfile);
+        ivToggleMenu.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (drawer.isDrawerOpen(GravityCompat.END)){
+            drawer.closeDrawer(GravityCompat.END);
+        }else {
+            drawer.openDrawer(GravityCompat.END);
+        }
     }
 }
